a30acb9e80c8029ee1904b7e63cf4ff88054166e
Mayank Kumar <mayankdas99@gmail.com>
2024-09-27 22:09:57 +0530

    token_utils_upadated_cors-policy_27092024

--- Backend/utils/setTokensCookies.js
+++ Backend/utils/setTokensCookies.js
@@ -1,29 +1,37 @@
-const setTokensCookies = (res, accessToken, refreshToken, newAccessTokenExp, newRefreshTokenExp) => {
-  const accessTokenMaxAge = (newAccessTokenExp - Math.floor(Date.now() / 1000)) * 1000;
-  const refreshTokenmaxAge = (newRefreshTokenExp - Math.floor(Date.now() / 1000)) * 1000;
+const setTokensCookies = (
+  res,
+  accessToken,
+  refreshToken,
+  newAccessTokenExp,
+  newRefreshTokenExp
+) => {
+  const accessTokenMaxAge =
+    (newAccessTokenExp - Math.floor(Date.now() / 1000)) * 1000;
+  const refreshTokenmaxAge =
+    (newRefreshTokenExp - Math.floor(Date.now() / 1000)) * 1000;
 
   // Set Cookie for Access Token
-  res.cookie('accessToken', accessToken, {
+  res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: true, // Set to true for production (HTTPS)
     maxAge: accessTokenMaxAge,
-    sameSite: 'strict', // Adjust to 'lax' if your frontend requires cross-site requests
+    sameSite: "none", // Adjust to 'lax' if your frontend requires cross-site requests
   });
 
   // Set Cookie for Refresh Token
-  res.cookie('refreshToken', refreshToken, {
+  res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: true, // Set to true for production (HTTPS)
     maxAge: refreshTokenmaxAge,
-    sameSite: 'strict', // Adjust to 'lax' if needed
+    sameSite: "none", // Adjust to 'lax' if needed
   });
 
   // Set Cookie for is_auth (accessible via client-side)
-  res.cookie('is_auth', true, {
+  res.cookie("is_auth", true, {
     httpOnly: false, // Allows client-side JavaScript access
     secure: true, // Set to true for production (HTTPS)
     maxAge: refreshTokenmaxAge,
-    sameSite: 'strict', // Adjust according to your cross-site requirements
+    sameSite: "none", // Adjust according to your cross-site requirements
   });
 };
 
